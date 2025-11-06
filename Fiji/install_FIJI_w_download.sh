31c44c34e911c33f79d2ee78809d5699e8560e49
Todd Fallesen <49306167+todd-fallesen@users.noreply.github.com>
2025-11-06 13:32:28 +0000

    Update install_FIJI_w_download.sh
    updated to work in folders that have spaces

--- Fiji/install_FIJI_w_download.sh
+++ Fiji/install_FIJI_w_download.sh
@@ -2,14 +2,14 @@
 
 #get file from https://downloads.micron.ox.ac.uk/fiji_update/mirrors/fiji-latest/fiji-linux64.zip
 
-current_directory=$(pwd)
+current_directory="$(pwd)"
 FIJI_URL="https://downloads.imagej.net/fiji/archive/20250514-1117/fiji-linux64.zip"
-filename=$(basename "$FIJI_URL")
+filename="$(basename "$FIJI_URL")"
 
 wget -O "$current_directory/$filename" "$FIJI_URL"
 
 #unzip the file
-unzip $filename
+unzip "$filename"
 
 #remove the zip file
 rm -r fiji-linux64.zip
@@ -20,16 +20,17 @@ cd Fiji.app
 #modify the permissions on Fiji launcher
 chmod u+x ImageJ-linux64
 
-current_directory=$(pwd)
+current_directory="$(pwd)"
 
 # Create a file in the home directory to save the current directory
-output_file="$HOME/Run_Fiji.sh"
+output_file="$HOME/Run_Fiji_test.sh"
 
-echo "cd $current_directory/" > "$output_file"
-echo "./ImageJ-linux64" >> "$output_file"
+{
+  echo "cd \"$current_directory\""
+  echo "./ImageJ-linux64"
+} > "$output_file"
 
 echo "File created at: $output_file"
 
 cd $HOME
 chmod u+x $output_file
-
