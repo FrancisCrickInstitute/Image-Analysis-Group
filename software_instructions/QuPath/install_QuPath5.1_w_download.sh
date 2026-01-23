aeee0c85bb634d3404bf1e1ca57cfa5ae411265a
Todd Fallesen <49306167+todd-fallesen@users.noreply.github.com>
2026-01-23 11:36:03 +0000

    fixed download
    Added a function to remove the downloaded tar file after extraction and updated the script to create a launcher in the home directory.

--- software_instructions/QuPath/install_QuPath5.1_w_download.sh
+++ software_instructions/QuPath/install_QuPath5.1_w_download.sh
@@ -1,6 +1,7 @@
 \#!/bin/bash
 
 #get file from https://github.com/qupath/qupath/releases/download/v0.5.1/QuPath-v0.5.1-Linux.tar.xz
+#written by Todd Fallesen.  Updated by Todd Fallesen Jan 23, 2026
 
 current_directory=$(pwd)
 QuPath_URL="https://github.com/qupath/qupath/releases/download/v0.5.1/QuPath-v0.5.1-Linux.tar.xz"
@@ -11,19 +12,33 @@ wget -O "$current_directory/$filename" "$QuPath_URL"
 #unzip the file
 tar -xf QuPath*
 
+#remove the zip file
+remove_if_exists() {
+    local file="$1"
+
+    if [[ -f "$file" ]]; then
+        rm "$file"
+        echo "Removed $file"
+    else
+        echo "File not found, skipping removal: $file"
+    fi
+}
+remove_if_exists "$current_directory/QuPath-v0.5.1-Linux.tar.xz"
+
+
 #change directory to the QuPath/bin
-cd $(ls -d QuPath*)
-cd bin
+cd QuPath-v0.5.1-Linux/QuPath/bin
+
 
 #modify the permissions on QuPath launcher
 chmod u+x QuPath
 
 current_directory=$(pwd)
-rm 
+ 
 # Create a file in the home directory to save the current directory
 output_file="$HOME/Run_QuPath.sh"
 
-echo "cd $current_directory/" > "$output_file"
+echo "cd $HOME/" > "$output_file"
 echo "./QuPath" >> "$output_file"
 
 echo "File created at: $output_file"
