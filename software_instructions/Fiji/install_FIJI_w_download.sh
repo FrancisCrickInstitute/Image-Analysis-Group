c7c06b42018bbba2be3627b17d83e7e345cb9f32
Dave Barry <david.barry@crick.ac.uk>
2026-05-25 09:13:51 +0100

    Update install_FIJI_w_download.sh

--- software_instructions/Fiji/install_FIJI_w_download.sh
+++ software_instructions/Fiji/install_FIJI_w_download.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#get file from https://downloads.micron.ox.ac.uk/fiji_update/mirrors/fiji-latest/fiji-linux64.zip
-
 current_directory="$(pwd)"
 FIJI_URL="https://downloads.imagej.net/fiji/archive/20250514-1117/fiji-linux64.zip"
 filename="$(basename "$FIJI_URL")"
